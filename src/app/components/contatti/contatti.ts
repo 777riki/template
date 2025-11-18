c341e3f5ba50ee9e6123ccf9ddc48d3f9f7c643e
Riccardo Calabri <riccardo.calabri@icloud.com>
2025-11-18 23:25:54 +0100

    feat: add EmailJS integration for contact form and enhance UI components

--- src/app/components/contatti/contatti.ts
+++ src/app/components/contatti/contatti.ts
@@ -1,11 +1,62 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import emailjs from '@emailjs/browser';
 
 @Component({
   selector: 'app-contatti',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contatti.html',
-  styleUrl: './contatti.css',
+  styleUrls: ['./contatti.css'],
 })
 export class Contatti {
 
+  name: string = '';
+  email: string = '';
+  object: string = '';
+  message: string = '';
+
+  showAlert: boolean = false;
+  alertMessage: string = '';
+
+  async send(name: string, email: string, object: string, message: string): Promise<void> {
+    // Controllo campi vuoti
+    if (!this.name.trim() || !this.email.trim() || !this.object.trim() || !this.message.trim()) {
+      this.alertMessage = '❌ Devi compilare tutti i campi!';
+      this.showAlert = true;
+      return;
+    }
+
+    try {
+      const response = await emailjs.send(
+        "service_884q5m4",
+        "template_9zggoca",
+        {
+          name: this.name,
+          time: new Date().toLocaleString(),
+          message: this.message,
+          email: this.email,
+          subject: this.object
+        },
+        { publicKey: 'lEcvSC2l4Fhqgg5ws' }
+      );
+
+      // Reset form
+      this.name = '';
+      this.email = '';
+      this.object = '';
+      this.message = '';
+
+      this.alertMessage = '✅ Email inviata con successo!';
+      this.showAlert = true;
+
+    } catch (error) {
+      console.error(error);
+      this.alertMessage = '❌ Qualcosa è andato storto. Riprova più tardi.';
+      this.showAlert = true;
+    }
+  }
+
+  closeAlert() {
+    this.showAlert = false;
+  }
 }
